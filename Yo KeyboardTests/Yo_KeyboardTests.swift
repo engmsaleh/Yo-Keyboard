43b6f97fdd4c6b39d11d5f0f666cd6b4fdd9bbe6
Cameron Nottingham <cjnott@Camerons-MacBook-Air.local>
2014-06-19 23:08:11 -0500

    Initial Commit

---
+++ Yo KeyboardTests/Yo_KeyboardTests.swift
@@ -0,0 +1,35 @@
+//
+//  Yo_KeyboardTests.swift
+//  Yo KeyboardTests
+//
+//  Created by Cameron Nottingham on 6/19/14.
+//  Copyright (c) 2014 Cameron Nottingham. All rights reserved.
+//
+
+import XCTest
+
+class Yo_KeyboardTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
